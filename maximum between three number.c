4ecb6c828ae02acd7a873daa4e66aa4d278d7589
Shadia Islam Shipu <144358112+shadiashipu@users.noreply.github.com>
2023-09-12 11:47:14 -0700

    Update maximum between three number.c

--- maximum between three number.c
+++ maximum between three number.c
@@ -5,15 +5,17 @@ int main() {
     int num1, num2, num3;
     scanf("%d %d %d", &num1, &num2, &num3);
 
-    if(num1 > num2) {
+    if(num1 > num2 && num1 > num3) {
         printf("%d is Maximum", num1);
     }
-    else if(num2 > num3) {
+    if(num2 > num1 && num2 > num3) {
         printf("%d is Maximum", num2);
     }
-    else if(num3 > num1) {
+    if(num3 > num1 && num3 > num2) {
         printf("%d is Maximum", num3);
     }
 
+
     return 0;
 }
+
