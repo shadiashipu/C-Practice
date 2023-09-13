ed7a9ae395806f2f43b24d01f8420d0f5eb2a1e5
Shadia Islam Shipu <144358112+shadiashipu@users.noreply.github.com>
2023-09-13 10:49:12 -0700

    Create number of days in month.c

---
+++ number of days in month.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    if(n == 1 || n == 3 || n == 5 || n == 7 || n == 10 || n == 12) {
+        printf("31");
+    }
+    else if(n == 4 || n == 6 || n == 8 || n == 9 || n == 11) {
+        printf("30");
+    }
+    else if(n == 2) {
+        printf("28");
+    }
+    return 0;
+}
