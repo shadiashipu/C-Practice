b2a2bd193647480ac9799154b96b4947ce2dee2b
Shadia Islam Shipu <144358112+shadiashipu@users.noreply.github.com>
2023-11-24 21:00:42 +0600

    Create student result.c

---
+++ student result.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    int num1, num2, num3, num4, num5, num6, num7, num8;
+
+    printf("Physics: ");
+    scanf("%d", &num1);
+    printf("Chemistry: ");
+    scanf("%d", &num2);
+    printf("Biology: ");
+    scanf("%d", &num3);
+    printf("Higher Math: ");
+    scanf("%d", &num4);
+    printf("Information & Tecnology: ");
+    scanf("%d", &num5);
+    printf("Islam & Moral Education: ");
+    scanf("%d", &num6);
+    printf("Bangladesh & World: ");
+    scanf("%d", &num7);
+    printf("Agriculture Studies: ");
+    scanf("%d", &num8);
+
+    if(num1 == 80 && num == 100) {
+        printf("Grade: A+\n Point: 5");
+    }
+
+
+}
