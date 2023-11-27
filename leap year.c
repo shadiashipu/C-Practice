03819c33bb88d36d41839004c462f386ae9ec4f0
Shadia Islam Shipu <144358112+shadiashipu@users.noreply.github.com>
2023-11-27 20:03:57 +0600

    Create leap year.c

---
+++ leap year.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int y;
+    printf("Enter the year to cheak: ");
+    scanf("%d", &y);
+
+    if(((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0)) {
+        printf("It is a leap year");
+    }
+    else{
+        printf("It is not a leap year");
+    }
+
+    return 0;
+}
