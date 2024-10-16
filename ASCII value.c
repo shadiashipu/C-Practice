0f1ad721770a8dd741655969186f61a205546c0e
Shadia Islam Shipu <144358112+shadiashipu@users.noreply.github.com>
2024-10-16 23:25:15 +0600

    Create ASCII value.c

---
+++ ASCII value.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main(){
+
+    int x;
+    printf("Enter the number: ");
+    scanf("%d", &x);
+
+    if(x == 0){
+      printf("48");
+    }
+    else if(x == 1){
+       printf("49");
+    }
+    else if(x == 2){
+       printf("50");
+    }
+    else if(x == 3){
+       printf("51");
+    }
+    else if(x == 4){
+       printf("52");
+    }
+    else if(x == 5){
+       printf("53");
+    }
+    else if(x == 6){
+       printf("54");
+    }
+    else if(x == 7){
+       printf("55");
+    }
+    else if(x == 8){
+       printf("56");
+    }
+    else if(x == 9){
+       printf("57");
+    }
+
+
+    return 0;
+
+}
